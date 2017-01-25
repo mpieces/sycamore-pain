7a76a7835ba07cd9407c90e65c96a48a5a322cef
Marisa Ramon <mer0522@gmail.com>
2017-01-25 16:46:06 -0600

    Add content to basic pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/about' => 'static_pages#about'
   get '/providers' => 'static_pages#providers'
   get '/procedures' => 'static_pages#procedures'
+  get '/procedures/esi' => 'static_pages#esi'
   get '/contact' => 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
