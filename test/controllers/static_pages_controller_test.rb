441ba83ecdc780327414e4287c0ba561f4c8ab67
Marisa Ramon <mer0522@gmail.com>
2017-01-25 12:44:14 -0600

    Define full_title method in Application Helper file

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,37 +2,32 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
-
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
-    assert_select "title", "Home | Sycamore Pain Clinic"
+    assert_select "title", "Sycamore Pain Clinic"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | Sycamore Pain Clinic"
   end
 
   test "should get providers" do
-    get static_pages_providers_url
+    get providers_path
     assert_response :success
     assert_select "title", "Providers | Sycamore Pain Clinic"
   end
 
    test "should get procedures" do
-    get static_pages_procedures_url
+    get procedures_path
     assert_response :success
     assert_select "title", "Procedures | Sycamore Pain Clinic"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | Sycamore Pain Clinic"
   end
