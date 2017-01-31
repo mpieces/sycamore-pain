bbfbef1026c457b15bdc148b71f6f8f9004596dc
Marisa Ramon <mer0522@gmail.com>
2017-01-31 10:56:40 -0600

    Create working Request model with validations and tests

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,5 @@
 class Request
+  include ActiveModel::Model
+  attr_accessor :first_name, :last_name, :email, :phone, :comments
+  validates :first_name, :last_name, :email, :phone, :comments, presence: true
 end
--- test/models/request_test.rb
+++ test/models/request_test.rb
@@ -11,4 +11,31 @@ class RequestTest < ActiveSupport::TestCase
     assert req.respond_to?(:email), 'does not respond to :email'
     assert req.respond_to?(:comments), 'does not respond to :comments'
   end
+
+  test 'should be valid when all attributes are set' do
+    attrs = {
+      first_name: 'marisa',
+      last_name: 'ramon',
+      phone: '1234567890',
+      email: 'marisa@example.org',
+      comments: 'a;sldkfjas;ldfkjas;lf'
+    }
+
+    req = Request.new attrs
+    assert req.valid?, 'should be valid'
+  end
+
+  test 'name, phone, email, comments are required' do
+    req = Request.new
+
+    refute req.valid?, 'Blank Message should be invalid'
+
+    assert_match /blank/, req.errors[:first_name].to_s
+    assert_match /blank/, req.errors[:last_name].to_s
+    assert_match /blank/, req.errors[:phone].to_s
+    assert_match /blank/, req.errors[:email].to_s
+    assert_match /blank/, req.errors[:comments].to_s
+  end
+
 end
+
